72dcee4bacaa005161accc64029bdb007e41ac76
NickswiftTele2 <nikolay.bystrov@tele2.ru>
2024-09-30 11:52:00 +0300

    it_kama_01

---
+++ src/components/Content.jsx
@@ -0,0 +1,24 @@
+const Content = () => {
+    return (
+        <div className='content'>
+            <img src='https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg' />
+            <div>
+                ava
+            </div>
+            <div>
+                Post
+                <div>
+                    new Post
+                </div>
+                <div>
+                    Post1
+                </div>
+                <div>
+                    Post2
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Content
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://cdn.worldvectorlogo.com/logos/tele2.svg' />
+        </header>
+    )
+}
+
+export default Header
